750927a701930afb6290eef674d6c2aa34afd373
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-09 11:50:04 -0700

    Updates

---
+++ precache-manifest.fa1d5ed7ab3f14fb8b032b542e93f010.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddb794276780ce4f4dfd06e2dcf42b54",
+    "url": "/markdown-react/index.html"
+  },
+  {
+    "revision": "ec6df642e69a2edf39ba",
+    "url": "/markdown-react/static/css/main.bf94f707.chunk.css"
+  },
+  {
+    "revision": "33c7eba302bb3b412ee8",
+    "url": "/markdown-react/static/js/2.f05eae30.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/markdown-react/static/js/2.f05eae30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6df642e69a2edf39ba",
+    "url": "/markdown-react/static/js/main.426af459.chunk.js"
+  },
+  {
+    "revision": "e7d013566408c2983c8a",
+    "url": "/markdown-react/static/js/runtime-main.9653fe36.js"
+  }
+]);
